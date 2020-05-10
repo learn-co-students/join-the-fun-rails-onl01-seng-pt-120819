a0e932c7ef601490338681b8560a8b49f48cada4
dwanerich <dwanerich@gmail.com>
2020-05-10 14:14:40 -0400

    Done.

--- db/migrate/20140212204456_create_taxis.rb
+++ db/migrate/20140212204456_create_taxis.rb
@@ -2,6 +2,8 @@ class CreateTaxis < ActiveRecord::Migration
   def change
     create_table :taxis do |t|
       t.timestamps null: false
+      t.string :passenger_id
+      t.string :ride_id
     end
   end
 end
